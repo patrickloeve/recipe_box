a1bec1f746fd56c9ac3fa6b0a80a196f018e7660
patrickloeve <patrick_loeve@hotmail.com>
2015-11-12 15:50:24 +0100

    added project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecepisBox::Application.config.secret_key_base = 'b9c5387907300a3eaa20c16fb0b4a986e171aa0a342b2c5c40f1b49f6673d25625eff2be839fbdd1f4b283bca927b7a25b2a6265cd78e1154d65a3add401bd97'
